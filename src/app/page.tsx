29e1c91f278df153d97c8bab568c261d08326b11
agu <iba.agustin@gmail.com>
2024-09-27 10:48:31 -0300

    update pictures optional on db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,18 @@ const apiKey = process.env.API_SIGNATURE;
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen w-full flex-col items-center justify-between bg-[#2b2b36] p-24">
+    <main className="flex min-h-screen w-full flex-col items-center justify-center gap-4 bg-[#2b2b36] p-24">
       <a
         className="text-white text-xl p-2 rounded border-2 border-white"
         href="/exchange"
       >
         Comprar/vender crypto →
+      </a>
+      <a
+        className="text-white text-xl p-2 rounded border-2 border-white"
+        href="/auth/signin"
+      >
+        Ingresar →
       </a>
     </main>
   );
