a4317c857f0493bde053fddb4d873c5e5bb7d515
agu <iba.agustin@gmail.com>
2024-09-27 10:44:28 -0300

    merge setup

--- src/app/exclusive/page.tsx
+++ src/app/exclusive/page.tsx
@@ -14,20 +14,20 @@ export default async function ExchangePage() {
   const formInfo = await Promise.allSettled([
     getConstants(apiUrl!, apiKey!),
     getDollar(apiUrl!, apiKey!),
+    getAllConstants(apiUrl!, apiKey!),
   ]);
 
-  const [constantsResult, dollarResult] = formInfo;
-
-  const allConsts = await getAllConstants(apiUrl!, apiKey!);
-  console.log("all constants =>", allConsts);
+  const [constantsResult, dollarResult, allConstants] = formInfo;
 
   const constants =
     constantsResult.status == "fulfilled" && constantsResult.value;
 
-  console.log(constants);
   const dollarPrice = dollarResult.status == "fulfilled" && dollarResult.value;
+  const allConsts = allConstants.status == "fulfilled" && allConstants.value;
 
-  console.log(dollarPrice);
+  console.log("constants", constants);
+  console.log("all constants", allConsts);
+  console.log("dollarprice", dollarPrice);
   return (
     <main className="flex min-h-screen flex-col w-full items-center justify-between  bg-[#2b2b36] p-24 pt-4 lg:pt-12">
       <VipExchangeForm cryptos={allConsts} {...dollarPrice} />
