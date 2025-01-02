c3e1742f834a4c18879229f8217db29e54ee1aac
agu <iba.agustin@gmail.com>
2025-01-02 13:32:58 -0300

    fix middleware rerouting loop

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,32 @@
-// middleware.ts in project root
-import { type NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { isTokenValid } from "../utils/authHelpers";
 
 export function middleware(req: NextRequest) {
   const token = req.cookies.get("admin_session")?.value;
   const tokenObj = token && JSON.parse(token);
 
+  // If already on panel or login, proceed normally
+  if (
+    req.nextUrl.pathname === "/admin/panel" ||
+    req.nextUrl.pathname === "/admin-login"
+  ) {
+    return NextResponse.next();
+  }
+
   try {
     if (!token) throw new Error("No token");
-
     if (!isTokenValid(tokenObj)) throw new Error("Wrong user or password");
 
-    console.log("Credentials ok");
-    return NextResponse.next();
+    return NextResponse.redirect(new URL("/admin/panel", req.url), {
+      status: 307,
+    });
   } catch (error) {
-    return NextResponse.redirect(new URL("/admin-login", req.url));
+    return NextResponse.redirect(new URL("/admin-login", req.url), {
+      status: 307,
+    });
   }
 }
 
 export const config = {
-  matcher: "/admin/:path*",
+  matcher: ["/admin", "/admin/:path*"],
 };
